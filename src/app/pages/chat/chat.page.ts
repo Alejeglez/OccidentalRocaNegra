03af936db2e95cfb9dcbdb8b5c8082ff55330063
Alejandro <alejandrojeglezsantana@gmail.com>
2022-10-08 12:37:25 +0100

    Se modifica apartado chat

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -7,6 +7,7 @@ import { IonContent } from '@ionic/angular';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
+  
   messages = [
     {user: 'Roca Negra',
     createdAt: new Date().getTime(),
@@ -16,11 +17,30 @@ export class ChatPage implements OnInit {
   ];
   currentUser = 'Usuario';
   newMsg = '';
+  msg = 0;
   @ViewChild(IonContent) content:IonContent;
 
   constructor() { }
 
   ngOnInit() {
+    
+  }
+  response(){
+    var msgs:string[];
+    msgs = ['Nos quedan tres habitaciones libres',
+    'El horario de recepción es de 9:30 a 21:30',
+    'El desayuno hay que pagarlo aparte',
+    '¿Necesita algo más?',
+    'Gracias por comunicarse con nosotros',
+    'Disconnected!'
+  ]
+  this.messages.push({
+    user: 'Roca Negra',
+    createdAt: new Date().getTime(),
+    msg: msgs[this.msg]
+  })
+  this.msg = this.msg + 1;
+  return 0;
   }
 
   sendMessage(){
@@ -32,7 +52,9 @@ export class ChatPage implements OnInit {
 
     this.newMsg = '';
     this.content.scrollToBottom(200);
+    this.response();
+
   }
-  
+
 
 }
