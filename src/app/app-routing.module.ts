eca1519eaab9c55c7721f29669c9daf97452bb0c
Alejandro <alejandrojeglezsantana@gmail.com>
2022-10-11 20:23:20 +0100

    Versión final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -63,6 +63,10 @@ const routes: Routes = [
     path: 'sign-up',
     loadChildren: () => import('./pages/sign-up/sign-up.module').then( m => m.SignUpPageModule)
   },
+  {
+    path: 'opinion',
+    loadChildren: () => import('./pages/opinion/opinion.module').then( m => m.OpinionPageModule)
+  },
 ];
 
 @NgModule({
